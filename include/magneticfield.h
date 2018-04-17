849da42c49475a4b1c28ac4c7431146b2ed41c81
Carmelo Evoli <carmelo.evoli@gssi.infn.it>
2018-04-17 15:13:15 +0200

    rename and clean basic class

---
+++ include/magneticfield.h
@@ -0,0 +1,64 @@
+#ifndef __MAGNFIELD_H
+#define __MAGNFIELD_H
+
+#include <cstdlib>
+#include <iostream>
+#include <map>
+#include <vector>
+#include <cmath>
+
+#include "mks.h"
+#include "constants.h"
+
+inline double normVector(const std::vector<double>& v) {
+	return std::sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+}
+
+class MagneticField {
+
+private:
+	double Bmin = 0;
+	double Bmax = 0;
+
+public:
+	MagneticField() {
+	}
+
+	virtual ~MagneticField() {
+	}
+
+	std::vector<double> GetBperp(const double& x, const double& y, const double& z);
+
+	//double GetBtransverse(const double&, const double&, const double&);
+
+	virtual std::vector<double> GetB(const double& x, const double& y, const double& z) {
+		return std::vector<double>(3, 0);
+	}
+};
+
+class ConstantField: public MagneticField {
+
+private:
+	double BConstant = 1. * muG;
+
+public:
+	ConstantField() {
+	}
+
+	ConstantField(const double& B_) {
+		BConstant = B_;
+	}
+
+	virtual ~ConstantField() {
+	}
+
+	virtual std::vector<double> GetB(const double& x, const double& y, const double& z) {
+			std::vector<double> Bret;
+			Bret.push_back(BConstant / std::sqrt(3.0));
+			Bret.push_back(BConstant / std::sqrt(3.0));
+			Bret.push_back(BConstant / std::sqrt(3.0));
+			return Bret;
+	}
+};
+
+#endif
