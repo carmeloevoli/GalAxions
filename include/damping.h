65dbbff155939dcd25eb7615264e66cb92a4503f
Carmelo Evoli <carmelo.evoli@gssi.infn.it>
2018-06-06 18:56:50 +0200

    include damping - to be fixed

---
+++ include/damping.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDE_DAMPING_H_
+#define INCLUDE_DAMPING_H_
+
+// damping
+//		const double x = (Energy / eV) / 15.4;
+//				(WithDamping_) ?
+//						45.57e-24
+//								* (1.0 - 2.003 * pow(x, -0.5) - 4. / 806. / x + 50.577 * pow(x, -1.5) - 171.044 * pow(x, -2) + 231.608 * pow(x, -2.5)
+//										- 81.885 * pow(x, -3)) / pow(EnergyInEv / 1e3, 3.5) :
+//						0.0; // CHECK DIMENSION!
+//		const double XsecHI = (WithDamping_) ? XsecH2 / 2.833 : 0.0; // CHECK DIMENSION!
+
+#endif /* INCLUDE_DAMPING_H_ */
