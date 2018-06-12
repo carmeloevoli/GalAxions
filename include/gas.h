113c3f783ff00d6e0f4157544fbb16606028a523
Carmelo Evoli <carmelo.evoli@me.com>
2018-06-12 15:59:29 +0200

    reformat gas

--- include/gas.h
+++ include/gas.h
@@ -1,5 +1,5 @@
-#ifndef __GAS_DISTRIBUTION_H
-#define __GAS_DISTRIBUTION_H
+#ifndef GALCONDA_GAS_DISTRIBUTION_H_
+#define GALCONDA_GAS_DISTRIBUTION_H_
 
 #include <algorithm>
 #include <cmath>
@@ -11,29 +11,32 @@ extern "C" double ymw16_ne(double x_pc, double y_pc, double z_pc);
 
 class ElectronModel {
 public:
-	ElectronModel() {
-	}
-	virtual ~ElectronModel() {
-	}
-	virtual double get(const double& x, const double& y, const double& z) const = 0;
+    ElectronModel() {
+    }
+
+    virtual ~ElectronModel() {
+    }
+
+    virtual double get(const double &x, const double &y, const double &z) const = 0;
 };
 
-class Cordes91: public ElectronModel {
+class Cordes91 : public ElectronModel {
 private:
-	double fne1 = 0.025 / cm3;
-	double H1 = 1.00 * kpc;
-	double A1 = 20.0 * kpc;
-	double fne2 = 0.200 / cm3;
-	double H2 = 0.15 * kpc;
-	double A2 = 2.0 * kpc;
-	double R2 = 4.0 * kpc;
+    double fne1 = 0.025 / cm3;
+    double H1 = 1.00 * kpc;
+    double A1 = 20.0 * kpc;
+    double fne2 = 0.200 / cm3;
+    double H2 = 0.15 * kpc;
+    double A2 = 2.0 * kpc;
+    double R2 = 4.0 * kpc;
+
 public:
-	double get(const double& x, const double& y, const double& z) const override;
+    double get(const double &x, const double &y, const double &z) const override;
 };
 
-class YMW16: public ElectronModel {
+class YMW16 : public ElectronModel {
 public:
-	double get(const double& x, const double& y, const double& z) const override;
+    double get(const double &x, const double &y, const double &z) const override;
 };
 
 #endif
--- src/gas.cpp
+++ src/gas.cpp
@@ -1,12 +1,12 @@
 #include "gas.h"
 
-double Cordes91::get(const double& x, const double& y, const double& z) const {
-	double r = std::sqrt(x * x + y * y);
-	double ne1 = fne1 * exp(-fabs(z) / H1) * exp(-pow2(r / A1));
-	double ne2 = fne2 * exp(-fabs(z) / H2) * exp(-pow2((r - R2) / A2));
-	return ne1 + ne2;
+double Cordes91::get(const double &x, const double &y, const double &z) const {
+    double r = std::sqrt(x * x + y * y);
+    double ne1 = fne1 * exp(-fabs(z) / H1) * exp(-pow2(r / A1));
+    double ne2 = fne2 * exp(-fabs(z) / H2) * exp(-pow2((r - R2) / A2));
+    return ne1 + ne2;
 }
 
-double YMW16::get(const double& x, const double& y, const double& z) const {
-	return ymw16_ne(x / pc, y / pc, z / pc) / cm3;
+double YMW16::get(const double &x, const double &y, const double &z) const {
+    return ymw16_ne(x / pc, y / pc, z / pc) / cm3;
 }
